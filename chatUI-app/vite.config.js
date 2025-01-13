32208234a07973440a15823e26b22113a1e03cd6
MaBokSing <karweng381@gmail.com>
2025-01-13 08:11:04 +0800

    Deploy Build
    - Just a try

--- chatUI-app/vite.config.js
+++ chatUI-app/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext'
+  },
 })
